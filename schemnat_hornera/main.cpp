08621f12cd77c4391f8da8d035ed3244d67ad113
Pomarancz <kuba.wtorkiewicz@gmail.com>
2022-02-09 10:05:09 +0100

    Initial Commit

---
+++ schemnat_hornera/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  schemnat_hornera
+//
+//  Created by Kuba Wtorkiewicz on 09/02/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
