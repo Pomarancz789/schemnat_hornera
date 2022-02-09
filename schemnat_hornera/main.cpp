c76174473d2017ec55522a9857d13987dc9dd8b4
Pomarancz <kuba.wtorkiewicz@gmail.com>
2022-02-09 10:29:35 +0100

    popełniam na funkcji

--- schemnat_hornera/main.cpp
+++ schemnat_hornera/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int horner_iteracyjny(int tab[], int stopien, int x)
+{
+    int temp=tab[0];
+    for(int i=0; i<stopien; i++)
+    {
+        temp*=x;
+        temp+=tab[i+1];
+    }
+    return temp;
+}
+
 int main() {
     //2x^3-3x^2+6x-1
     //x*(x*(2x-3)+6)-1
@@ -8,13 +19,14 @@ int main() {
     int tab[]={2,-3,6,-1};
     int stopien=3;
     
-    int temp=tab[0];
-    for(int i=0; i<stopien; i++)
-    {
-        temp*=x;
-        temp+=tab[i+1];
-    }
-    cout << temp << endl;
+    //int temp=tab[0];
+    //for(int i=0; i<stopien; i++)
+    //{
+        //temp*=x;
+        //temp+=tab[i+1];
+    //}
+    //cout << temp << endl;
+    cout << horner_iteracyjny(tab, stopien, x)<<endl;
     
     return 0;
 }
